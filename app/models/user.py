4907547f4e05b9071d639cbb11cb59cf012fbf14
MaxBakshaev <risen.2011@mail.ru>
2025-12-16 11:40:59 +0900

    Added base and user models, libs alembic passlib[bcrypt] python-jose[cryptography] email-validator

---
+++ app/models/user.py
@@ -0,0 +1,45 @@
+"""
+Docs:
+https://docs.sqlalchemy.org/en/20/orm/quickstart.html
+"""
+
+from datetime import datetime
+
+from sqlalchemy import String, Boolean, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(
+        String(320),
+        unique=True,
+        index=True,
+        nullable=False,
+    )
+    hashed_password: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False,
+    )
+
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        default=True,
+        nullable=False,
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
